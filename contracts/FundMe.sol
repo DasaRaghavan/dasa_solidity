09ab875f4eae5ea3029cf772b33bb20af1d56f01
DasaRaghavan <dashfamily@gmail.com>
2025-11-23 12:50:36 -0500

    getting it right

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -10,7 +10,6 @@ contract FundMe {
     // Enable contract owner to withdraw deposited funds from the contract
     // Set minimum funding requirements (1 ether)
     // Two primary functions: fund, withdraw
-    // this version includes three types of withdraw functions -- Transfer, Send, Call
     
     uint constant MINIMUM_USD = 5;
     address[] funders;
@@ -30,7 +29,32 @@ contract FundMe {
         
         funders.push(msg.sender); // track funders
         addressFundedAmount[msg.sender] = fundsReceived; // track USD by sender
-        owner.transfer(msg.value); // transfer to owner immediately
+
+        // The following transfer is inefficient in gas usage
+        // will replace with withdraw functions
+        // owner.transfer(msg.value); // transfer to owner immediately 
+
+    }
+
+    function withdrawWithTransfer() public payable {
+        owner.transfer(address(this).balance);  // will withdraw all accumulated balance in the contract to the owner
+                                                // has a gas limit of 2300
+    }
+
+    function withdrawWithSend() public payable {
+        bool success = owner.send(address(this).balance);   // will withdraw all accumulated balance in the contract to the owner
+                                                            // has a gas limit of 2300
+                                                            // handle the bool return code to validate success
+        require(success, "Send failed!");
+    }
+
+    function withdrawWithCall() public payable {
+        (bool success, ) = owner.call{value: address(this).balance}("");    // call is flexible and powerful
+                                                                            // will withdraw all accumulated balance in the contract to the owner
+                                                                            // has no gas limit
+                                                                            // can call any function in the contract with a function name (without an ABI)
+                                                                            // handle the bool return code to validate success
+        require(success, "Send with call failed!");
     }
 
     function getBalance() public view returns(uint) {
